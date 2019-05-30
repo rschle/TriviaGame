9afd0dd623769ffffa030ed995f3f61a5d83862d
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-05-29 20:58:10 -0700

    added start donut/button thing

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    //Make it so that when you click the start button, the questions are shown 
+    $("#donutStart").on("click", function(){
+        console.log("i've been clicked");
+    })
+//clicking the start button also needs to activate the timer (going down from 120 seconds)
+//code for the questions and choices
+	//make it so that the user cannot choose more than one answer for each question
+	//specify the correct answer for each of the questions
+//once the the timer ends
+	//print “Time’s up!”
+	//show how many of the questions the user answers correctly (see if user input matches the correct 
+	    //answers you stored 
+    //probably will have to make a variable that counts how many answers the user got correct 			(initially set it to 0, then you’ll use if/else statements or some sort of function to add to the 			score) 
+
+})
